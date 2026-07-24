92ecf86de3321d0fa8e3de6915adce2a181ecc44
govchecker <webmaster@hegeralarm.de>
2026-07-24 16:50:27 +0200

    update sdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.mysudoku"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.mysudoku"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
@@ -32,6 +32,9 @@ android {
     }
     buildFeatures {
         compose = true
+    }
+    lint {
+        disable += "EditedTargetSdkVersion"
     }
 }
 
